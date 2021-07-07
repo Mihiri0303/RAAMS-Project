2b45ba8f5e2a6582b6e0e11264bfeab2b1093904
Mihiri0303 <93081@fhss.sjp.ac.lk>
2021-07-07 22:00:50 +0530

    UI updated

--- Front-end/src/components/login.js
+++ Front-end/src/components/login.js
@@ -8,23 +8,20 @@ const Login = () => {
             <>
              <section className='container'>
                 <div className='row'>
-                    <div className="col-md-6 ">
+                    <div className="col-md-4 offset-md-1 ">
                         <div className="d-flex flex-column gap-2 justify-content-center align-items-center">
-                        <h4>Welcome</h4>
-                        <p>Log in to your account</p> 
-                        </div>
-                        <div className="row g-3 px-md-5 px-sm-3">
+                            <h4>Welcome</h4>
+                            <p>Log in to your account</p> 
                             <input className={`form-control shadow-sm border-0 py-2"}`} style={{fontSize:"0.8rem"}} placeholder="User Name" />
                             <input className={`form-control shadow-sm border-0 py-2 "}`} style={{fontSize:"0.8rem"}} placeholder="Password" />
-                        <div className="d-flex gap-2 justify-content-center align-items-center">
-                            <button className="btn btn-sm px-3 py-1 btn-primary">Login</button>
+                            <div className="d-flex gap-2 justify-content-center align-items-center">
+                                <button className="btn btn-sm px-4 py-2 btn-primary">Login</button>
+                            </div>
                         </div>
-                         </div>
                     </div>
-                    <div className="col-md-6">
+                    <div className="col-md-6 offset-md-1">
                         <img src={imge} style={{height : '100%',width : 400}}/>
                     </div>
-                   
                 </div>
             </section>  
             </>
--- Front-end/src/components/signup.js
+++ Front-end/src/components/signup.js
@@ -8,25 +8,27 @@ const SignUp = () => {
                 <div className='row'>
                     <div className="col-md-6 ">
                         <div className="d-flex flex-column justify-content-center align-items-center">
-                        <h4>Welcome</h4>
-                        <p>Sign up to your account</p>                        
+                            <h4>Welcome</h4>
+                            <p>Sign up to your account</p>                        
+                            <div className="row gap-2 px-md-5 px-sm-3 w-75">
+                                <input className={`form-control shadow-sm border-0 py-2 "}`} style={{fontSize:"0.8rem"}} placeholder="First Name" />
+                                <input className={`form-control shadow-sm border-0 py-2 "}`} style={{fontSize:"0.8rem"}} placeholder="last Name" />
+                                <input className={`form-control shadow-sm border-0 py-2 "}`} style={{fontSize:"0.8rem"}} placeholder="Mobile" />
+                                <select className={`form-select shadow-sm border-0 py-2 `} defaultValue={0} style={{fontSize:"0.8rem"}}>
+                                    <option value="0">Normal User</option>
+                                    <option value="0">Owner and User</option>
+                                </select>
+                                <input className={`form-control shadow-sm border-0 py-2 "}`} style={{fontSize:"0.8rem"}} placeholder="E-mail" />
+                                <input className={`form-control shadow-sm border-0 py-2 "}`} style={{fontSize:"0.8rem"}} placeholder="Password" />
+                                <input className={`form-control shadow-sm border-0 py-2 "}`} style={{fontSize:"0.8rem"}} placeholder="Confirm Password" />
+                            <div className="">
+                                <input className={`form-check-input`} type="checkbox"></input><span className="ps-2 " style={{fontSize:"0.8rem"}}>Agree with terms and conditions</span>
+                            </div>
+                            <div className="d-flex justify-content-center align-items-center">
+                                <button className="btn btn-sm px-5 py-2 btn-primary">Sign Up</button>
+                            </div>
+                            </div>
                         </div>
-                        <div className="row g-2 px-md-5 px-sm-3">
-                            <input className={`form-control shadow-sm border-0 py-2 "}`} style={{fontSize:"0.8rem"}} placeholder="First Name" />
-                            <input className={`form-control shadow-sm border-0 py-2 "}`} style={{fontSize:"0.8rem"}} placeholder="last Name" />
-                            <input className={`form-control shadow-sm border-0 py-2 "}`} style={{fontSize:"0.8rem"}} placeholder="Mobile" />
-                            <select className={`form-select shadow-sm border-0 py-2 `} defaultValue={0} style={{fontSize:"0.8rem"}}></select>
-                            <input className={`form-control shadow-sm border-0 py-2 "}`} style={{fontSize:"0.8rem"}} placeholder="E-mail" />
-                            <input className={`form-control shadow-sm border-0 py-2 "}`} style={{fontSize:"0.8rem"}} placeholder="Password" />
-                            <input className={`form-control shadow-sm border-0 py-2 "}`} style={{fontSize:"0.8rem"}} placeholder="Confirm Password" />
-                        <div className="">
-                            <input className={`form-check-input`} type="checkbox"></input><span className="ps-2 " style={{fontSize:"0.8rem"}}>Agree with terms and conditions</span>
-                        </div>
-                        <div className="d-flex justify-content-center align-items-center">
-                            <button className="btn btn-sm px-3 py-1 btn-primary">Sign Up</button>
-                        </div>
-                        </div>
-                        
                     </div>
                     <div className="col-md-6">
                         <img src={imge} style={{height : '100%',width : 400}}/>
