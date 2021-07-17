283ec8ad50cb5a467725058ba075b5e709609d3c
Mihiri0303 <93081@fhss.sjp.ac.lk>
2021-07-17 23:02:37 +0530

    login form validation

--- Front-end/src/components/login.js
+++ Front-end/src/components/login.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import imge from './../asset/login.jpg';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+
+
+const SigninSchema = Yup.object().shape({
+    UserName: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    Password: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required')
+  });
 
 
 const Login = () => {
@@ -9,15 +23,38 @@ const Login = () => {
              <section className='container'>
                 <div className='row'>
                     <div className="col-md-4 offset-md-1 ">
-                        <div className="d-flex flex-column gap-2 justify-content-center align-items-center">
-                            <h4>Welcome</h4>
-                            <p>Log in to your account</p> 
-                            <input className={`form-control shadow-sm border-0 py-2"}`} style={{fontSize:"0.8rem"}} placeholder="User Name" />
-                            <input className={`form-control shadow-sm border-0 py-2 "}`} style={{fontSize:"0.8rem"}} placeholder="Password" />
-                            <div className="d-flex gap-2 justify-content-center align-items-center">
-                                <button className="btn btn-sm px-4 py-2 btn-primary">Login</button>
-                            </div>
-                        </div>
+                        <Formik
+                            initialValues={{
+                                UserName: '',
+                                Password: ''
+                            }}
+                            validationSchema={SigninSchema}
+                            onSubmit={values => {
+                                console.log(values);
+                            }}>
+                            {({ errors, touched }) => (
+                                <Form className="d-flex flex-column gap-2 justify-content-center align-items-center needs-validation">
+                                    <h4>Welcome</h4>
+                                    <p>Log in to your account</p> 
+                                    <div className="col-md-12 position-relative mb-2">
+                                        <Field name="UserName" type="text" className={`form-control shadow-sm border-0 py-2 ${errors.UserName && touched.UserName &&  "is-invalid"}`} style={{fontSize:"0.8rem"}} placeholder="User Name" />
+                                        <div className="invalid-feedback">
+                                            Please provide a valid Username.
+                                        </div>
+                                    </div>
+                                    <div className="col-md-12 position-relative mb-2">
+                                        <Field name="Password" type="password" className={`form-control shadow-sm border-0 py-2 ${errors.Password && touched.Password && "is-invalid"}`} style={{fontSize:"0.8rem"}} placeholder="Password" />
+                                        <div className="invalid-feedback">
+                                            Please provide a valid Password.
+                                        </div>
+                                    </div>
+
+                                    <div className="d-flex gap-2 justify-content-center align-items-center">
+                                        <button type="submit" className="btn btn-sm px-4 py-2 btn-primary">Login</button>
+                                    </div>
+                                </Form>
+                            )}
+                        </Formik>
                     </div>
                     <div className="col-md-6 offset-md-1">
                         <img src={imge} style={{height : '100%',width : 400}}/>
