566141cdc04c232905961f11009b759379b0082e
Mihiri0303 <93081@fhss.sjp.ac.lk>
2021-07-17 10:24:03 +0530

    Config mongodb

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const { MongoClient } = require('mongodb');
 const acco = require('./routes/accommodation')
 const app = express()
 const port = 8000
+
+
+const uri = "mongodb+srv://mihiri0303:Udara0303@raamsdb.hsngu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(async (err) => {
+  const collection = await client.db("test").collection("devices").find();
+    await collection.forEach(data => console.log(data))
+  // perform actions on the collection object
+  client.close();
+});
 
 
 app.use('/accommodation',acco);
