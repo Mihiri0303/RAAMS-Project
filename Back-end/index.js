d71548bc48044077a322a46ef554f4eb7532d51e
Mihiri0303 <93081@fhss.sjp.ac.lk>
2021-07-21 22:46:33 +0530

    add /api prefix to routes

--- Back-end/index.js
+++ Back-end/index.js
@@ -3,19 +3,18 @@ const cors = require('cors');
 
 const acco = require('./Api/accommodation')
 const auth = require('./Api/auth')
-const app = express()
 
+const app = express()
 const port = 8000
 
 app.use(cors());
 
 app.use(express.json())
-app.use(auth)
-app.use('/accommodation',acco)
+const apiRoute = express.Router();
+apiRoute.use('/',auth)
+apiRoute.use('/accommodation',acco)
 
-app.get('/',(req,res) => {
-    res.send('Hello World!')
-})
+app.use('/api',apiRoute);
 
 app.listen(port,() => {
     console.log(`API listening at http://localhost:${port}`)
