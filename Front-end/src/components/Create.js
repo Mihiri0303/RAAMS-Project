7aefdb73fb473e8e1c92730ef7853753aeeabae1
Mihiri0303 <93081@fhss.sjp.ac.lk>
2021-07-21 23:15:33 +0530

    Add validations for forms

--- Front-end/src/components/Create.js
+++ Front-end/src/components/Create.js
@@ -1,14 +1,25 @@
 import React ,{ useState, useEffect, useRef} from 'react';
 import mapboxgl from 'mapbox-gl';
 import { Link } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+
+const CreateSchema = Yup.object().shape({
+    Tittle : Yup.string().required(),
+    Type : Yup.string().required(),
+    Address : Yup.string().required(),
+    Latitude : Yup.string().required(),
+    Longitude : Yup.string().required(),
+    Amount_Perday : Yup.number().required(),
+});
 
 const Create = () => {
 
     const mapContainer = useRef();
-    const [lnglat, setLngLat] = useState([79.8998759,6.8278084]);
-    const [zoom, setZoom] = useState(8);
-    const [Locations,setLocations] = useState([]);
-    const [map,setMap] = useState();
+    const [lnglat, setLngLat] = useState([79.8998759,6.8278084]); // eslint-disable-line
+    const [zoom, setZoom] = useState(8); // eslint-disable-line
+    const [Locations,setLocations] = useState([]); // eslint-disable-line
+    const [map,setMap] = useState(); // eslint-disable-line
     mapboxgl.accessToken = 'pk.eyJ1IjoibGFzaXRoODc5IiwiYSI6ImNrbjV2eW1tcTA4N2IycnM0eDY4c2xuZ3QifQ.1b2qEsuBFBVNg682HGe7hw';
 
     useEffect(() => {
@@ -37,7 +48,7 @@ const Create = () => {
         
         setMap(map);
         return () => map.remove();
-    },[]);
+    },[]); // eslint-disable-line
         return (
             <>
             <section className='container'>
@@ -46,46 +57,60 @@ const Create = () => {
                         <Link to="/ownrAcc" className="btn btn-sm btn-outline-success fw-bold ps-4 pe-4" >Back</Link>
                     </div>
                     <div className="col-md-4 col-sm-12 ">
-                        <form className="col-12 p-4 rounded">
+                    <Formik 
+                        initialValues={{
+                            Tittle: '',
+                            Type : '',
+                            Address : '',
+                            Latitude : '',
+                            Longitude :'',
+                          Amount_Perday : '',
+                        }}
+                        validationSchema={CreateSchema}
+                        onSubmit={values => {
+                            console.log(values);
+                        }}>
+                        {({errors,touched}) => (
+
+                        <Form className="col-12 p-4 rounded">
                             <h5 className="fw-bolder">Add Accommodation</h5>
                             <div className="col-md-12 my-3">
-                                <input type="text" className={`form-control shadow-sm border-0 `}  style={{fontSize:"0.8rem"}} placeholder="Title *" />
+                                <Field name="TIttle" type="text" className={`form-control shadow-sm border-0 ${errors.Tittle && touched.Tittle && "is-invalid"}`}  style={{fontSize:"0.8rem"}} placeholder="Title *" />
                                 <input type="text" hidden style={{fontSize:"0.8rem"}} />
                                 <div className="invalid-feedback">Title is required.</div>
                             </div>
                             <div className="col-md-12 my-3">
-                                <input type="text" className={`form-control shadow-sm border-0 `}  style={{fontSize:"0.8rem"}} placeholder="Type *" />
+                                <Field name="Type" type="text" className={`form-control shadow-sm border-0 ${errors.Type && touched.Type && "is-invalid"}`}  style={{fontSize:"0.8rem"}} placeholder="Type *" />
                                 <div className="invalid-feedback">Type is required.</div>
                             </div>
                             <div className="col-md-12 my-3">
-                                <input type="text" className={`form-control shadow-sm border-0 `}  style={{fontSize:"0.8rem"}} placeholder="Address *" />
+                                <Field name="Address" type="text" className={`form-control shadow-sm border-0 ${errors.Address && touched.Address && "is-invalid"}`}  style={{fontSize:"0.8rem"}} placeholder="Address *" />
                                 <div className="invalid-feedback">Address is required.</div>
                             </div>
                             <div className="row g-3 mb-3">
                                 <div className="col-md-6">
-                                    <input type="text" className={`form-control shadow-sm border-0 `}  style={{fontSize:"0.8rem"}} placeholder="Latitude" />
+                                    <Field name="latitude" type="text" className={`form-control shadow-sm border-0 ${errors.Latitude && touched.Latitude && "is-invalid"}`}  style={{fontSize:"0.8rem"}} placeholder="Latitude" />
                                     <div className="invalid-feedback">
                                         Enter valid Latitude.
                                     </div>
                                 </div>
                                 <div className="col-md-6">
-                                    <input type="text" className={`form-control shadow-sm border-0 `}  style={{fontSize:"0.8rem"}} placeholder="Longitude"  />
+                                    <Field name="Longitude" type="text" className={`form-control shadow-sm border-0 ${errors.Longitude && touched.Longitude && "is-invalid"} `}  style={{fontSize:"0.8rem"}} placeholder="Longitude"  />
                                     <div className="invalid-feedback">Enter valid Longitude.</div>
                                 </div>
                             </div>
                             <div className="col-md-12 my-3">
-                                <input type="number" className={`form-control shadow-sm border-0 `}  style={{fontSize:"0.8rem"}} placeholder="Amount Per Day *" />
+                                <Field name="Amount_Perday" type="number" className={`form-control shadow-sm border-0 ${errors.Amount_Perday && touched.Amount_Perday && "is-invalid"}`}  style={{fontSize:"0.8rem"}} placeholder="Amount Per Day *" />
                                 <div className="invalid-feedback">Enter valid Amount.</div>
                             </div>
                             <button className="btn btn-sm btn-primary w-100"><b>Save</b></button>
-                        </form>
+                        </Form>
+                    )}</Formik>
                     </div>
                     <div className="col-md-8 col-sm-12 rounded-end" style={{height: "65vh"}} id="Managemap" ref={mapContainer}/>
                 </div>
             </section>
           </>
-              
-               
         )
 }
 
--- Front-end/src/components/login.js
+++ Front-end/src/components/login.js
@@ -5,9 +5,7 @@ import * as Yup from 'yup';
 
 
 const SigninSchema = Yup.object().shape({
-    UserName: Yup.string()
-      .min(2, 'Too Short!')
-      .max(50, 'Too Long!')
+    Email: Yup.string().email()
       .required('Required'),
     Password: Yup.string()
       .min(2, 'Too Short!')
@@ -24,7 +22,7 @@ const Login = () => {
                     <div className="col-md-4 offset-md-1 ">
                         <Formik
                             initialValues={{
-                                UserName: '',
+                                Email: '',
                                 Password: ''
                             }}
                             validationSchema={SigninSchema}
@@ -36,9 +34,9 @@ const Login = () => {
                                     <h4>Welcome</h4>
                                     <p>Log in to your account</p> 
                                     <div className="col-md-12 position-relative mb-2">
-                                        <Field name="UserName" type="text" className={`form-control shadow-sm border-0 py-2 ${errors.UserName && touched.UserName &&  "is-invalid"}`} style={{fontSize:"0.8rem"}} placeholder="User Name" />
+                                        <Field name="Email" type="text" className={`form-control shadow-sm border-0 py-2 ${errors.Email && touched.Email &&  "is-invalid"}`} style={{fontSize:"0.8rem"}} placeholder="Email" />
                                         <div className="invalid-feedback">
-                                            Please provide a valid Username.
+                                            Please provide a valid Email.
                                         </div>
                                     </div>
                                     <div className="col-md-12 position-relative mb-2">
