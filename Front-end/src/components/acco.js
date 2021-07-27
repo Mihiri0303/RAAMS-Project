d367aa993494d06e48edf94b1cfd56ea5763720d
Mihiri0303 <93081@fhss.sjp.ac.lk>
2021-07-27 19:07:02 +0530

    bug fixes

--- Front-end/src/components/acco.js
+++ Front-end/src/components/acco.js
@@ -1,7 +1,7 @@
 import React,{useEffect,useState} from 'react'
 import axios from 'axios';
 import { connect } from 'react-redux';
-import { useHistory  } from 'react-router';
+import { useHistory  } from 'react-router-dom';
 
 
 
